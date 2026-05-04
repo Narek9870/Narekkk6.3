1ba3816ccde902cfa7d7ededb11156323a3e687b
Narek9870 <narek.ogannisyan.82@bk.ru>
2026-05-04 18:04:25 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/narekkk/MainActivity.kt
+++ app/src/main/java/com/example/narekkk/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Инициализируем наш модуль и передаем ему Контекст (чтобы DataStore работал)
+        // Инициализируем наш модуль и передаем ему(чтобы DataStore работал)
         AppModule.init(applicationContext)
 
         enableEdgeToEdge()
